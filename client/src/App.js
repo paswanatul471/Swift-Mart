d04d7b5e341498bb824e1ff16067ee0c2b064d89
Atul Paswan <90141410+paswanatul471@users.noreply.github.com>
2023-08-28 12:44:51 +0530

    import all file in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/header/Header'
+import { Box} from '@mui/material'
+import Home from './components/home/Home'
+import DataProvider from './context/DataProvider'
+import DetailsView from './components/details/DetailsView'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Cart from './components/cart/Cart'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DataProvider>
+      <BrowserRouter>
+        <Header/>
+        <Box style={{marginTop: 54}}>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/product/:id' element={<DetailsView/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+          </Routes>
+          
+        </Box>
+    </BrowserRouter>
+    </DataProvider>
+  )
 }
 
-export default App;
+export default App
