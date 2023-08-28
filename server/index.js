4fec898022a59863ac0d50dd48af8cab820ac159
Atul Paswan <90141410+paswanatul471@users.noreply.github.com>
2023-08-28 16:42:35 +0530

    uploaded

--- server/index.js
+++ server/index.js
@@ -9,7 +9,15 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://swift-mart-backend.vercel.app/"],
+        methods: ['POST', 'GET'],
+        credentials: true
+    }
+
+));
+app.use(express.json());
 app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use('/', Router);
